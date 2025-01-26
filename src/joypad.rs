cf251788d57035d0b15bc9809034ef1546af70ce
Leo <boskeroni@gmail.com>
2025-01-26 16:59:48 +0000

    actually finished work on the joypad

--- src/joypad.rs
+++ src/joypad.rs
@@ -28,26 +28,7 @@ pub fn joypad_press(input: winit::keyboard::KeyCode, mem: &mut Memory) {
     }
     mem.write_io(JPRegisters::KeyInput as u32, joypad);
 
-    let control = mem.read_u16(JPRegisters::KeyCnt as u32);
-    if (control >> 14) & 1 == 1 {
-        let mask = control & 0x3FF;
-        let keys = joypad & mask;
-
-        let call_interrupt;
-        let interrupt_condition = (control >> 15) & 1 == 1;
-        match interrupt_condition {
-            true => call_interrupt = mask == keys,
-            false => call_interrupt = keys != 0,
-        }
-
-        let mut i_flag = mem.read_u16(0x4000202);
-        i_flag &= !(1 << 12);
-        i_flag |= (call_interrupt as u16) << 12;
-
-        mem.write_io(0x4000202, i_flag); 
-    }
-
-    mem.write_io(JPRegisters::KeyCnt as u32, control);
+    joypad_interrupt(mem, joypad);
 }
 
 pub fn joypad_release(input: winit::keyboard::KeyCode, mem: &mut Memory) {
@@ -68,4 +49,28 @@ pub fn joypad_release(input: winit::keyboard::KeyCode, mem: &mut Memory) {
         _ => {}
     }
     mem.write_io(JPRegisters::KeyInput as u32, joypad);
+    joypad_interrupt(mem, joypad);
+}
+
+fn joypad_interrupt(mem: &mut Memory, joypad: u16) {
+    let control = mem.read_u16(JPRegisters::KeyCnt as u32);
+    if (control >> 14) & 1 == 1 {
+        let mask = control & 0x3FF;
+        let keys = joypad & mask;
+
+        let call_interrupt;
+        let interrupt_condition = (control >> 15) & 1 == 1;
+        match interrupt_condition {
+            true => call_interrupt = mask == keys,
+            false => call_interrupt = keys != 0,
+        }
+
+        let mut i_flag = mem.read_u16(0x4000202);
+        i_flag &= !(1 << 12);
+        i_flag |= (call_interrupt as u16) << 12;
+
+        mem.write_io(0x4000202, i_flag); 
+    }
+
+    mem.write_io(JPRegisters::KeyCnt as u32, control);
 }
