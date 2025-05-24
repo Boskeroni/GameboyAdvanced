aa5c2cd47fa8e71d8c117dbe189af6a6be0f5ec1
Leo <boskeroni@gmail.com>
2025-05-24 16:01:30 +0100

    added more debug functions

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,5 @@
-use core::{gba_frame, joypad::{joypad_press, joypad_release}, ppu::Ppu};
+use core::{cpu::Cpu, gba_frame, joypad::{joypad_press, joypad_release}, ppu::Ppu};
+use std::time::Instant;
 
 // just contains all of the debug code
 use eframe::egui;
@@ -21,17 +22,27 @@ struct GbaAdvanceDebug {
     show_cpu_panel: bool,
     show_instruction_panel: bool,
     gba_context: GbaContext,
+
+    paused: bool,
+    step: bool,
+    last_render: Instant,
 }
 impl GbaAdvanceDebug {
     pub fn new(mut context: GbaContext) -> Self {
         core::prelimenary(&mut context.memory);
 
         Self {
-            show_memory_panel: false,
-            show_vram_panel: false,
-            show_cpu_panel: false,
-            show_instruction_panel: false,
+            show_memory_panel: true,
+            show_vram_panel: true,
+            show_cpu_panel: true,
+            show_instruction_panel: true,
             gba_context: context,
+
+            // this controls the flow of the gba
+            paused: true,
+            step: false,
+
+            last_render: Instant::now(),
         }
     }
 }
@@ -39,51 +50,88 @@ impl GbaAdvanceDebug {
 impl eframe::App for GbaAdvanceDebug {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // this always gets called so its chill
-        game_panel(ctx, &mut self.gba_context);
+        if !self.paused || self.step {
+            game_panel(ctx, &mut self.gba_context);
+            self.step = false;
+        }
+        draw(&self.gba_context.ppu, ctx);
 
         // options panel
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Debug panel");
 
-            ui.checkbox(
-                &mut self.show_memory_panel,
-                "Show memory panel",
-            );
-            ui.checkbox(
-                &mut self.show_vram_panel,
-                "Show VRAM panel",
-            );
-            ui.checkbox(
-                &mut self.show_cpu_panel,
-                "Show CPU panel",
-            );
-            ui.checkbox(
-                &mut self.show_instruction_panel,
-                "Show instruction panel",
-            );
+            // menu to create new windows with information
+            ui.checkbox(&mut self.show_memory_panel, "Show memory panel");
+            ui.checkbox(&mut self.show_vram_panel,"Show VRAM panel");
+            ui.checkbox(&mut self.show_cpu_panel,"Show CPU panel");
+            ui.checkbox(&mut self.show_instruction_panel,"Show instruction panel");
 
+            // pause and step buttons
+            let pause_button_text = match self.paused {
+                false => "⏸",
+                true => "⏵"
+            };
+            if ui.add(egui::Button::new(pause_button_text)).clicked() {
+                self.paused = !self.paused;
+            }
+
+            if ui.add(egui::Button::new("⏭")).clicked() {
+                self.step = true;
+            }
+
+
+            let diff = Instant::now().duration_since(self.last_render).as_nanos();
+            let fps = 1_000_000_000 / diff;
+            ui.label(format!("{fps} fps achieved"));
+            self.last_render = Instant::now();
         });
 
-        if self.show_memory_panel      { memory_panel(ctx);     }
-        if self.show_cpu_panel         { cpu_panel(ctx);        }
-        if self.show_vram_panel        { vram_panel(ctx);       }
-        if self.show_instruction_panel { instruction_panel(ctx);}
+        if self.show_memory_panel      { memory_panel(ctx)     }
+        if self.show_cpu_panel         { cpu_panel(ctx, &self.gba_context.cpu)}
+        if self.show_vram_panel        { vram_panel(ctx)       }
+        if self.show_instruction_panel { instruction_panel(ctx)}
 
         egui::Context::request_repaint(ctx);
     }
 }
 
-fn memory_panel(_ctx: &egui::Context) {
-    todo!();
+fn memory_panel(ctx: &egui::Context) {
+    ctx.show_viewport_immediate(
+        egui::ViewportId::from_hash_of("memory panel"), 
+        egui::ViewportBuilder::default()
+            .with_title("memory panel")
+            .with_inner_size([400., 400.]),
+        |ctx, class| {
+            assert!(class == egui::ViewportClass::Immediate);
+            egui::CentralPanel::default().show(ctx, |ui| {
+
+            });
+        }
+    );
 }
-fn cpu_panel(_ctx: &egui::Context) {
-    todo!();
+fn cpu_panel(ctx: &egui::Context, cpu: &Cpu) {
+    ctx.show_viewport_immediate(
+        egui::ViewportId::from_hash_of("CPU panel"), 
+        egui::ViewportBuilder::default()
+            .with_title("Cpu panel")
+            .with_inner_size([200., 400.]),
+        |ctx, class| {
+            assert!(class == egui::ViewportClass::Immediate);
+
+            egui::CentralPanel::default().show(ctx, |ui| {
+                for i in 0..15 {
+                    let value = cpu.get_register(i);
+                    ui.code(format!("Reg {i}: {value:X}"));
+                }
+            });
+        } 
+    );
 }
 fn vram_panel(_ctx: &egui::Context) {
-    todo!();
+
 }
 fn instruction_panel(_ctx: &egui::Context) {
-    todo!();
+
 }
 
 fn game_panel(ctx: &egui::Context, gba_context: &mut GbaContext) {
@@ -108,10 +156,9 @@ fn game_panel(ctx: &egui::Context, gba_context: &mut GbaContext) {
             }
         }
     });
-
-    draw(&gba_context.ppu, ctx);
-
 }
+
+const SCREEN_RATIO: f32 = 2.0;
 fn draw(ppu: &Ppu, ctx: &egui::Context) {
     let converted_pixels = texture_pixels(ppu);
     let texture = ctx.load_texture(
@@ -126,11 +173,11 @@ fn draw(ppu: &Ppu, ctx: &egui::Context) {
         egui::ViewportId::from_hash_of("game_viewport"), 
         egui::ViewportBuilder::default()
             .with_title("game viewport")
-            .with_inner_size([SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32]),
+            .with_inner_size([(SCREEN_WIDTH as f32) * SCREEN_RATIO, (SCREEN_HEIGHT as f32) * SCREEN_RATIO]),
         |ctx, class| {
             assert!(class == egui::ViewportClass::Immediate);
             egui::CentralPanel::default().show(ctx, |ui| {
-                ui.add(egui::Image::new(sized_texture).fit_to_exact_size(size));
+                ui.add(egui::Image::new(sized_texture).fit_to_exact_size(size * 2.));
             });
         } 
     );
